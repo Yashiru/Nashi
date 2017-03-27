ad4495436d4dda0813a07bab5a983e89ad46b19d
léo fasano <leo.fasano@hortis.ch>
2017-03-27 09:55:28 +0200

    clean archi

--- nashi/src/app/app.module.ts
+++ nashi/src/app/app.module.ts
@@ -1,19 +1,11 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 import { SpeechRecognitionPage  } from '../pages/ServiceSpeechRecognition/SpeechRecognitionPage'
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
     SpeechRecognitionPage,
 
   ],
@@ -23,10 +15,6 @@ import { SpeechRecognitionPage  } from '../pages/ServiceSpeechRecognition/Speech
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
     SpeechRecognitionPage,
 
   ],
