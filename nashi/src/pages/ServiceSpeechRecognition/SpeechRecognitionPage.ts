df54cb1a1312b7486fede6b6160509f3a1244001
leo <fasanol.info@gmail.com>
2017-04-01 13:13:05 +0200

    merge text to speech into devlop

--- nashi/src/pages/ServiceSpeechRecognition/SpeechRecognitionPage.ts
+++ nashi/src/pages/ServiceSpeechRecognition/SpeechRecognitionPage.ts
@@ -2,20 +2,22 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NashiBot } from '../../services/NashiBot.service';
 
-declare var SpeechRecognition: any;
 
 @Component({
     selector: 'speech-recognition-module',
     templateUrl: 'SpeechRecognitionPage.html'
 })
 export class SpeechRecognitionPage {
-
+    private speekresult: string;
     constructor(public navCtrl: NavController, private bot: NashiBot) {
 
     }
 
     public sayHello(){
-      this.bot.speek("Hi, i'm the nashi's bot, you can call me Nashi");
+      this.bot.sayToBot((result: string): void =>{
+        this.speekresult = result;
+        this.bot.speek(result);
+      })
     }
 
 }
--- nashi/src/services/NashiBot.service.ts
+++ nashi/src/services/NashiBot.service.ts
@@ -1,11 +1,25 @@
 import { TextToSpeech } from '@ionic-native/text-to-speech';
 import {Injectable} from "@angular/core";
+import { Platform } from 'ionic-angular';
+
+declare var SpeechRecognition: any;
 
 @Injectable()
 export class NashiBot {
+  recognition: any;
 
-  constructor(private tts: TextToSpeech){
+  constructor(private tts: TextToSpeech, platform: Platform){
+    platform.ready().then(() => {
+      this.recognition = new SpeechRecognition();
+      this.recognition.lang = 'en-US';
+      this.recognition.onnomatch = (event => {
+        console.log('No match found.');
+      });
+      this.recognition.onerror = (event => {
+        console.log('Error happens.');
+      });
 
+    });
   }
 
   public speek(message: string): void{
@@ -14,7 +28,15 @@ export class NashiBot {
       .catch((reason: any) => console.log(reason));
   }
 
-  public sayToBot(message: String): String{
+  public sayToBot(callback: (event: string) => void): String{
+    this.recognition.onresult = (event => {
+      if (event.results.length > 0) {
+        //TODO: send to wit service
+        callback(event.results[0][0].transcript);
+      }
+    });
+
+    this.recognition.start();
 
     return "";
   }
