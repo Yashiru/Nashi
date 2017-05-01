d9f7944a36e971785b517a863615f7c64906ed27
leo <fasanol.info@gmail.com>
2017-05-01 14:56:29 +0200

    init auth service

--- nashi/src/app/app.module.ts
+++ nashi/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { SpeechRecognitionPage  } from '../pages/ServiceSpeechRecognition/SpeechRecognitionPage'
 import { TextToSpeech } from '@ionic-native/text-to-speech';
 import { LoginPage } from '../pages/loginPage/login-page';
 
@@ -10,7 +9,6 @@ import { NashiBot } from '../services/NashiBot.service';
 @NgModule({
   declarations: [
     MyApp,
-    SpeechRecognitionPage,
     LoginPage,
 
   ],
@@ -20,7 +18,6 @@ import { NashiBot } from '../services/NashiBot.service';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    SpeechRecognitionPage,
     LoginPage,
 
   ],
