3a9287a88d5f132dfe7922dc0976874aa126fb0f
léo fasano <leo.fasano@hortis.ch>
2017-05-11 19:45:00 +0200

    no message

--- nashi/src/services/Wit-ai.service.ts
+++ nashi/src/services/Wit-ai.service.ts
@@ -15,23 +15,24 @@ export class WitAiService {
 
   }
 
-  public sayToBot(message : String, callback: (ingredients: Ingredient[]) => void): void{
+  public sayToBot(message : String, callback: (datas:any, ingredients: any) => void): void{
     let url: string = "http://leo-fasano.com/nashi/web/wit?message=" + message;
     let headers = new Headers({ 'Access-Control-Allow-Origin': "localhost:8100"});
 
     let options = new RequestOptions({ headers: headers });
 
     this.http.get(url).map(res => res.json()).subscribe(data => {
-      callback(this.ingrediantFactorise(data));
+      var isRecipes: Boolean;
+      if (data.ingredient) {
+        isRecipes = false;
+      }
+      else{
+        isRecipes = true;
+      }
+
+      callback(isRecipes, data);
     });
 
-  }
-
-  private ingrediantFactorise(jsonData: any): Ingredient[]{
-    console.log(jsonData);
-
-    let ing: Ingredient[] = this.witIngrediant.CreateIngrediants(jsonData);
-    return ing;
   }
 
 }
