56d1f04d4b48f61587abd5b833c300dfb80f74d2
jérôme GALLEGO <jeromegallego2@gmail.com>
2017-06-02 23:31:03 +0200

    affiche listing recipes

--- nashi/src/pages/recipes/recipes-page.ts
+++ nashi/src/pages/recipes/recipes-page.ts
@@ -14,10 +14,7 @@ export class RecipesPage {
     let datas = this.navParams.get("recipes");
     let i = 0;
     for (let data of datas.resources) {
-      console.log('------');
-      console.log(data.picture_url);
       if (typeof data.picture_url !== 'undefined' && data.picture_url.toString() != '' ) {
-        let url: any = "http://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg";
         let datas: any = {
           "name":  data.name,
           "url": data.picture_url
